84d7d6a99690b437f71c49c533d51b3b1c89a48f
Facundo Sole <facundojsole@gmail.com>
2025-10-05 18:10:43 -0300

    agregar conexion a base de datos neo4j

--- TPO-Progra-III/src/main/java/com/example/TPO_Progra_III/TpoPrograIiiApplication.java
+++ TPO-Progra-III/src/main/java/com/example/TPO_Progra_III/TpoPrograIiiApplication.java
@@ -1,5 +1,7 @@
 package com.example.TPO_Progra_III;
 
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.GraphDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,15 @@ public class TpoPrograIiiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpoPrograIiiApplication.class, args);
+
+		final String dbUri = "neo4j+s://6f05a98a.databases.neo4j.io";
+		final String dbUser = "neo4j";
+		final String dbPassword = "9udAN6IwIGmZj7r8uNtAtz__DPf61SPJHJz0LwrXfn8";
+
+		try (var driver = GraphDatabase.driver(dbUri, AuthTokens.basic(dbUser, dbPassword))) {
+			driver.verifyConnectivity();
+			System.out.println("Connection established.");
+		}
 	}
 
 }
