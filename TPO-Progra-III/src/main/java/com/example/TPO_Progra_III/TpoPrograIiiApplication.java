3674352e1f483d7d452f2209c34a1887c0b53a33
Paula Batalla <90630187+PaulaBatalla@users.noreply.github.com>
2025-10-05 17:55:23 -0300

    Creación de proyecto con Spring boot

---
+++ TPO-Progra-III/src/main/java/com/example/TPO_Progra_III/TpoPrograIiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.TPO_Progra_III;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoPrograIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpoPrograIiiApplication.class, args);
+	}
+
+}
---
+++ TPO-Progra-III/src/test/java/com/example/TPO_Progra_III/TpoPrograIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TPO_Progra_III;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoPrograIiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
