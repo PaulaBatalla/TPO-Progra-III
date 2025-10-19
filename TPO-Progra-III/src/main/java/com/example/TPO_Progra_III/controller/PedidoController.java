795e9cd93cab8d0003b638e913ddb9ececfc4147
Facundo Sole <facundojsole@gmail.com>
2025-10-19 20:58:35 -0300

    agregar link del front a la politica de CORS

--- TPO-Progra-III/src/main/java/com/example/TPO_Progra_III/controller/PedidoController.java
+++ TPO-Progra-III/src/main/java/com/example/TPO_Progra_III/controller/PedidoController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/pedidos")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://analisis-de-algoritmos.vercel.app"})
 public class PedidoController {
     private final ServicioOrdenamiento servicioOrdenamiento;
 
